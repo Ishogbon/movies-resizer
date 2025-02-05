9628a82cf39a157b0529a986530c1dc428a4dc06
Daniel Shogbon <ishogbon@gmail.com>
2025-02-05 18:23:35 +0100

    adding parser to txt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,71 @@
 use clap::Parser;
-use std::{fs, process::Command};
+use std::{
+    fs::{self, File},
+    io::{BufReader, Read},
+    process::Command,
+};
 
 #[derive(Parser)]
 struct Args {
     source: String,
     destination: String,
+}
+
+fn get_video_dimensions(file_path: &str) -> (u32, u32) {
+    let output = Command::new("ffprobe")
+        .arg("-v")
+        .arg("error")
+        .arg("-select_streams")
+        .arg("v:0")
+        .arg("-show_entries")
+        .arg("stream=width,height")
+        .arg("-of")
+        .arg("default=noprint_wrappers=1:nokey=1")
+        .arg(file_path)
+        .output()
+        .expect("Failed to query Video Dimensions");
+
+    if !output.status.success() {
+        panic!("Command failed to query video dimensions");
+    }
+
+    let output_str =
+        String::from_utf8(output.stdout).expect("Unable to parse string from dimensions std out");
+    let results: Vec<&str> = output_str.split_whitespace().collect();
+
+    if results.len() == 2 {
+        let width = results[0].parse::<u32>().unwrap();
+        let height = results[1].parse::<u32>().unwrap();
+        return (width, height);
+    }
+    panic!("Something went wrong parsing dimensions");
+}
+
+fn read_buffer(file_path: &str, width: u16, height: u16) {
+    let scale: usize = width as usize * height as usize * 3;
+
+    let file = File::open(file_path).expect("Unable to read file");
+    let mut reader = BufReader::new(file);
+
+    let mut buffer = vec![0u8; scale];
+
+    let mut frame_number = 0;
+
+    while reader.read_exact(&mut buffer).is_ok() {
+        frame_number += 1;
+
+        // Example: Get the top-left pixel of the frame
+        let r = buffer[0];
+        let g = buffer[1];
+        let b = buffer[2];
+
+        println!(
+            "Frame {} - First pixel R: {}, G: {}, B: {}",
+            frame_number, r, g, b
+        );
+    }
+
+    println!("Finished reading {} frames", frame_number);
 }
 
 fn process_video(input: &str, output: &str) {
@@ -37,5 +98,6 @@ fn main() {
         destination.push('/');
         destination.push_str(file.file_name().to_str().unwrap());
         process_video(file.path().to_str().unwrap(), destination.as_str());
+        read_buffer(&destination);
     });
 }
